12892b0121f9bc97e97aeaaa69722c1882b3c614
Maks <maks.chojniak@gmail.com>
2022-12-17 11:40:17 +0100

    Click Detector + End Bot Loop

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -8,12 +8,19 @@ public class Follower : MonoBehaviour
     public PathCreator pathCreator;
     public float speed = 5;
     float distanceTravelled;
+    [SerializeField] Transform endPosition;
 
     private void Update()
     {
-        distanceTravelled += speed * Time.deltaTime;
+        distanceTravelled += speed * 5 * Time.deltaTime;
         transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
         transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+
+        if(Vector3.Distance(this.transform.position, endPosition.position) < 1)
+        {
+            Debug.Log("End");
+            Destroy(this.gameObject);
+        }
 
     }
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform capsule;
 
-    // Update is called once per frame
+    [SerializeField] bool click = false;
+
+
     void Update()
     {
-        
+        if(click == false)
+            StartCoroutine(Click());
+
     }
+
+    IEnumerator Click()
+    {
+        if (Input.touchCount > 0)
+        {
+            click = true;
+
+            Vector3 pos = Input.GetTouch(0).position;
+
+            Ray ray = Camera.main.ScreenPointToRay(pos);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 1000, 1 << 6))
+            {
+                capsule.position = new Vector3(hit.point.x, 1f, hit.point.z);
+
+            }
+
+            yield return new WaitForSeconds(0.1f);
+
+            click = false;
+
+        }
+    }
+
+
 }
