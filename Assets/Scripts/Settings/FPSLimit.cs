6c4bc9e63d92c1bfef9d9f0b86f7b91faafed838
Maks <maks.chojniak@gmail.com>
2023-11-11 18:19:35 +0100

    Add FPS Limit And Slider Bar Logic

---
+++ Assets/Scripts/Settings/FPSLimit.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace DefaultNamespace
+{
+    [Serializable]
+    public struct FPSData
+    {
+        public int FPSValue;
+    }
+    
+    public class FPSLimit : MonoBehaviour
+    {
+        public FPSData[] FPSDatas;
+        [SerializeField] TMP_Text FPSLimitText;
+
+        [SerializeField] CanvasGroup LeftButton;
+        [SerializeField] CanvasGroup RightButton;
+
+        [Space(18)]
+        [Header("Debug")]
+        [SerializeField] int currentDataIndex;
+        
+
+        void Awake()
+        {
+            currentDataIndex = FPSDatas.Length - 1;
+            OnDataChanged();
+        }
+
+        public void FPSDataChange(int direction)
+        {
+            if (currentDataIndex + direction < 0)
+            {
+                // currentDataIndex = FPSDatas.Length - 1;
+            }
+            else if (currentDataIndex + direction >= FPSDatas.Length)
+            {
+                // currentDataIndex = 0;
+            }
+            else
+            {
+                currentDataIndex += direction;
+            }
+
+            OnDataChanged();
+        }
+
+        void OnDataChanged()
+        {
+            if (currentDataIndex <= 0)
+            {
+                LeftButton.interactable = false;
+                LeftButton.alpha = 0.7f;
+            }
+            else if (currentDataIndex >= FPSDatas.Length - 1)
+            {
+                RightButton.interactable = false;
+                RightButton.alpha = 0.7f;
+            }
+            else
+            {
+                LeftButton.interactable = true;
+                RightButton.interactable = true;
+                LeftButton.alpha = 1f;
+                RightButton.alpha = 1f;
+            }
+            
+            int FPSLimitValue = FPSDatas[currentDataIndex].FPSValue;
+            FPSLimitText.text = $"{(FPSLimitValue <= 0 ? "Unlimited" : $"{FPSLimitValue}")} FPS";
+
+            Application.targetFrameRate = FPSLimitValue;
+        }
+    }
+}
---
+++ Assets/Scripts/Settings/SettingsSliderBar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class SettingsSliderBar : MonoBehaviour
+    {
+        // public delegate void OnPointerHold(int value);
+        // OnPointerHold onPointerHold;
+
+        public event Action<int> onPointerHold;
+        const float interval = 0.25f;
+        
+        [SerializeField] Slider slider;
+
+        [SerializeField] float _interval;
+        bool readyToNextInvoke;
+        int _cachedSliderDirection;
+
+        public void OnBeginHold(int value)
+        {
+            _interval = interval;
+            readyToNextInvoke = true;
+            
+            _cachedSliderDirection = value;
+            
+            onPointerHold += OnHold;
+        }
+
+        public void OnEndHold()
+        {
+            _interval = interval;
+            readyToNextInvoke = false;
+            
+            _cachedSliderDirection = 0;
+            
+            onPointerHold -= OnHold;
+        }
+
+        void OnHold(int value)
+        {
+            float sliderValue = ((float)value / 100f);
+
+            slider.value += sliderValue;
+            
+            Invoke(nameof(SetReadyInvoke), _interval);
+        }
+
+        void SetReadyInvoke() => readyToNextInvoke = true;
+
+        void Update()
+        {
+            if (readyToNextInvoke)
+            {
+                readyToNextInvoke = false;
+
+                if (_interval > 0.035f)
+                    _interval -= Time.deltaTime * 2;
+                
+                onPointerHold?.Invoke(_cachedSliderDirection);
+            }
+        }
+    }
+}
