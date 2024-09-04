d37489e5f20e0bf86d25ac580e03f9e40f77b3d7
Maks <maks.chojniak@gmail.com>
2024-09-04 11:18:22 +0200

    Add Social Button Class

---
+++ Assets/Scripts/Social Medias/SocialMedia.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Social_Medias
+{
+    public class SocialMedia : MonoBehaviour
+    {
+        [SerializeField] string url = "";
+
+
+        public void Open()
+        {
+            if(string.IsNullOrEmpty(url))
+                return;
+            
+            Application.OpenURL(url);
+        }
+
+    }
+}
