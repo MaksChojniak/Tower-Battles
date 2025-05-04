48f267bca2e8e034fbab524046be926f672cfa16
Maks <maks.chojniak@gmail.com>
2025-05-04 21:39:38 +0200

    fix: change ads unit

--- Assets/Scripts/Ads/RewardedAd.cs
+++ Assets/Scripts/Ads/RewardedAd.cs
@@ -92,73 +92,18 @@ namespace Ads
 
         #region Unit IDs
 
-        const string TEST_UNIT = "ca-app-pub-3940256099942544/5354046379"; //ca-app-pub-3940256099942544/5224354917
+        const string TEST_UNIT = "ca-app-pub-3940256099942544/5354046379";
 
-        const string REWARDABLE_FULLSCREEN_NONE = "ca-app-pub-6306325732760549/9560036764";
-        const string REWARDABLE_FULLSCREEN_5_COINS = "ca-app-pub-6306325732760549/3493321869";
-        const string REWARDABLE_FULLSCREEN_10_COINS = "ca-app-pub-6306325732760549/5496668678";
-        const string REWARDABLE_FULLSCREEN_15_COINS = "ca-app-pub-6306325732760549/9839238368";
-        const string REWARDABLE_FULLSCREEN_20_COINS = "ca-app-pub-6306325732760549/5899993353";
-        const string REWARDABLE_FULLSCREEN_25_COINS = "ca-app-pub-6306325732760549/4586911689";
-        const string REWARDABLE_FULLSCREEN_30_COINS = "ca-app-pub-6306325732760549/2870505335";
-        const string REWARDABLE_FULLSCREEN_35_COINS = "ca-app-pub-6306325732760549/4608771020";
-        const string REWARDABLE_FULLSCREEN_40_COINS = "ca-app-pub-6306325732760549/3295689358";
-        const string REWARDABLE_FULLSCREEN_45_COINS = "ca-app-pub-6306325732760549/3856283945";
-        const string REWARDABLE_FULLSCREEN_50_COINS = "ca-app-pub-6306325732760549/1230120609";
+        const string REWARDABLE_FULLSCREEN = "ca-app-pub-6306325732760549/8243137278";
 
-        private string GetAdUnitID(Reward reward)
+        private string GetAdUnitID()
         {
-            return TEST_UNIT;
-            
-            string adUnit = REWARDABLE_FULLSCREEN_NONE;
-
-            if (reward.Type == RewardType.Coins)
-            {
-                switch (reward.Amount)
-                {
-                    case 5:
-                        adUnit = REWARDABLE_FULLSCREEN_5_COINS;
-                        break;
-                    case 10:
-                        adUnit = REWARDABLE_FULLSCREEN_10_COINS;
-                        break;
-                    case 15:
-                        adUnit = REWARDABLE_FULLSCREEN_15_COINS;
-                        break;
-                    case 20:
-                        adUnit = REWARDABLE_FULLSCREEN_20_COINS;
-                        break;
-                    case 25:
-                        adUnit = REWARDABLE_FULLSCREEN_25_COINS;
-                        break;
-                    case 30:
-                        adUnit = REWARDABLE_FULLSCREEN_30_COINS;
-                        break;
-                    case 35:
-                        adUnit = REWARDABLE_FULLSCREEN_35_COINS;
-                        break;
-                    case 40:
-                        adUnit = REWARDABLE_FULLSCREEN_40_COINS;
-                        break;
-                    case 45:
-                        adUnit = REWARDABLE_FULLSCREEN_45_COINS;
-                        break;
-                    case 50:
-                        adUnit = REWARDABLE_FULLSCREEN_50_COINS;
-                        break;
-                    default:
-                        adUnit = REWARDABLE_FULLSCREEN_NONE;
-                        break;
-                }
-            }
-            else if (reward.Type == RewardType.Experience) { }
-            else
-                adUnit = REWARDABLE_FULLSCREEN_NONE;
+            return TEST_UNIT; // For Tests time (closed and internal testing)
 
 #if UNITY_EDITOR
-            adUnit = TEST_UNIT;
+            return TEST_UNIT;
 #endif
-            return adUnit;
+            return REWARDABLE_FULLSCREEN;
         }
 
         #endregion
